b1a4529ffd49cd8d364db0a1aaa567d8fbe6ff37
himanshurohit11 <himanshurohit114@gmail.com>
2022-01-30 19:56:10 +0530

    This is odoo training developed estate module till wizard included

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':"Real Estate",
+    'category':'Sales',
+    'author':'Himanshu Rohit',
+    'application':True,
+    'data':[
+        'security/ir.model.access.csv',
+        'views/estate_property_view.xml',
+        'views/estate_property_menus_action.xml',
+        'wizard/offer_wizard_view.xml',
+    ],
+    'depends': ['base'],
+}
