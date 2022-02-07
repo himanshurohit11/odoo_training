062e57e427a0d330719fd29f96ea1823010996f2
himanshurohit11 <himanshurohit114@gmail.com>
2022-02-07 13:56:07 +0530

    [IMP] template

--- estate/controllers/main.py
+++ estate/controllers/main.py
@@ -3,15 +3,28 @@ from odoo.http import request
 
 
 class Estate(http.Controller):
-    @http.route('/',website=True,auth='public')
-    def estate_porperty_show(self,**kw):
-        # return "Hello This is property page"
+    @http.route(['/property','/property/static/<string:is_static>'],website=True,auth='public')
+    def estate_porperty_show(self,is_static=False, **kw):
 
-        properties = request.env['estate.property'].sudo().search([])
-        return request.render("estate.property",{
+        if is_static:
+            return request.render('estate.property_static', {
+                'properties': request.env['estate.property'].sudo().search([], limit=8)
+            })
 
-                'properties':properties,
-               
+        return request.render('estate.property', {
+                'properties': request.env['estate.property'].sudo().search([], limit=8)
+            })
 
 
-        })
+        # properties = request.env['estate.property'].sudo().search([])
+        # return request.render("estate.property",{
+        #         'properties':properties,
+        # })
+
+    @http.route(['/property/<model("estate.property"):property>', '/property/<string:is_static>'], auth="public", website=True)
+    def property_details(self, property=False, **kw):
+        if property:
+            return request.render('estate.property_details', {
+                'property': property,
+            })
+
