cfbc20624b567f96ce5099dd944a204b2f001cf3
himanshurohit11 <himanshurohit114@gmail.com>
2022-02-09 10:37:15 +0530

    [IMP] Java Script training

---
+++ JS Training/js/js3.js
@@ -0,0 +1,25 @@
+// premetive data types
+
+// string
+var n = 'Himanshu';
+console.log("my name is  "+n);
+console.log("my data type  is  "+(typeof n));
+// reference
+let myarr = [1,2,3,false,'string']
+console.log("my data type  is  "+(typeof myarr));
+
+// object literals
+let stMarks = {
+himanshu : 40,
+karan : 50
+}
+
+console.log("my data type  is  "+(typeof stMarks));
+
+function MyName() {
+    
+}
+console.log("my function data type  is  "+(typeof MyName));
+
+let date = new Date();
+ console.log(typeof date)
